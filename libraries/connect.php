ae4448238bdf3ddd8d48d2ba2249f4cf17225783
NGUYEN VAN DUC <vanducld@gmail.com>
2015-03-07 23:47:25 +0700

    user page

--- libraries/connect.php
+++ libraries/connect.php
@@ -1,10 +1,6 @@
 <?php
-//Require
-require 'configs/config.php';
+	$connect = mysql_pconnect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die('Not connected DB!');
+	$db = mysql_select_db(DB_DATABASE, $connect) or die('Not selected DB!');
 
-
-$connect = mysql_pconnect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die('Not connected DB!');
-$db = mysql_select_db(DB_DATABASE, $connect) or die('Not selected DB!');
-
-mysql_query('SET NAMES UTF8', $connect);
+	mysql_query('SET NAMES UTF8', $connect);
 ?>
